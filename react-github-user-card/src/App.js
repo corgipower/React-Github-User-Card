6a2e6a68f1d72ac2eac2e38e73e4842b3e199a90
Liz Shulman <corgipower@hotmail.com>
2020-08-08 10:20:31 -0400

    created app and made App.js a class

---
+++ react-github-user-card/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component() {
+  constructor(){
+    super();
+  }
+
+  render(){
+    return(
+      <div></div>
+    )
+  }
+}
+
+export default App;
