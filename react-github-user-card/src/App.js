652b9c79dc691864c46cb4e69a60b9b1d12f5be3
Liz Shulman <corgipower@hotmail.com>
2020-08-08 19:45:09 -0400

    everything is working, except for a warning about needing unique keys

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 import Card from './components/Card';
-
 class App extends React.Component {
   constructor() {
     super();
@@ -11,36 +10,26 @@ class App extends React.Component {
       followers: [],
     };
   }
-
   componentDidMount() {
     axios
       .get('https://api.github.com/users/corgipower')
       .then(res => {
         this.setState({user: res.data})
-        console.log(this.state.user);
       })
       .catch(err => console.log('user', err));
-
     axios
       .get('https://api.github.com/users/corgipower/followers')
       .then(res => {
         this.setState({followers: res.data})
-        console.log(this.state.followers)
       })
       .catch(err => console.log('followers', err));
   }
-
-  componentDidUpdate() {
-  }
-
   render(){
     return(
       <div>
-        <p>{this.state.user.login}</p>
         <Card user={this.state.user} followers={this.state.followers} />
       </div>
     )
   }
 }
-
 export default App;
--- react-github-user-card/src/components/Card.js
+++ react-github-user-card/src/components/Card.js
@@ -1,22 +1,29 @@
 import React from 'react'
+import './card.css'
 
-class Card extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            users: [],
-        };
-    }
+const Card = (props) => {
 
-    componentDidMount() {
-        console.log(this.props) //gives user and follower, but both are empty
-    }
+    const users = [props.user, ...props.followers]
 
-    render(){
-        return(
-            <div></div>
-        )
-    }
+     return(
+        <div>
+            {users.map(user => (
+                <div key={user.node_id} className="card">
+                    <img src={user.avatar_url} alt='profile avatar' />
+                    <div className="card-info">
+                        <h3 className="name">{user.name}</h3>
+                        <p className="username">{user.login}</p>
+                        <p>Location: {users.location}</p>
+                        <p>Profile:
+                            <a href={user.url}>{user.url}</a>
+                        </p>
+                        <p>Followers: {user.followers}</p>
+                        <p>Following: {users.following}</p>
+                        <p>Bio: {user.bio}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
 }
-
 export default Card;
